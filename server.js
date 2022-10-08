e641a104dc0e050bba2692acbae7cb02cf52487e
emriti <emriti@gmail.com>
2022-10-08 20:34:11 +0700

    edit server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/foo', routes); //to use the routes
+app.use('/', routes); //to use the routes
 
 const listener = app.listen(process.env.PORT || 3000, () => {
     console.log('Your app is listening on port ' + listener.address().port)
